9002b81caa17df70bff438153fa99287e7d69ef9
Prince <princeasa2005@gmail.com>
2023-08-11 21:54:42 +0000

    Genie1

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,11 @@
 #include<unistd.h>
 /**
  * main - Entry point
- * program to write something to screen
- * return: always 0(success)
+ * 
+ * Return: Always 0(success)
  */
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	/*return value indicates failure*/
 	return (1);
 }
