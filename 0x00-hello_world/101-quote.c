32297a898985bb1b950276dfa5a015f7705a61e5
Prince <princeasa2005@gmail.com>
2023-08-11 21:12:05 +0000

    Genie

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,10 @@
 /**
  * main - Entry point
  * program to write something to screen
- * retun: always 0(success)
+ * return: always 1(success)
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\ni", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
