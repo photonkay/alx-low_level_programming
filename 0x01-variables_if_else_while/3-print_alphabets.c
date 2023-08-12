8e602858ff067ac869b8c2ff8ebf35a4c7f19fab
Prince <princeasa2005@gmail.com>
2023-08-12 01:29:04 +0000

    Capitals

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * code to print upper and lower
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char me;
+
+	for (me = 'a'; me <= 'z'; me++)
+		putchar(me);
+	for (me = 'A'; me <= 'Z'; me++)
+		putchar(me);
+	putchar(me);
+	return (0);
+}
