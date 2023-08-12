845ff75e0cd291c88d169cce1532f35b1d0de703
Prince <princeasa2005@gmail.com>
2023-08-12 02:34:56 +0000

    Comp lang

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * code to print hexadecimal stuff
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+        int num;
+	char ch;
+
+        for (num = 48; num < 58; num++)
+        {
+                putchar(num);
+        }
+	for (ch = 'a'; ch <= 'f'; ch++)
+		putchar(ch);
+        putchar('\n');
+        return (0);
+}
+~
