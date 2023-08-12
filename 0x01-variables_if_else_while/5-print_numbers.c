76ab304ef4931d4442717d12c1ad6b84360083ac
Prince <princeasa2005@gmail.com>
2023-08-12 02:01:33 +0000

    Numbers:

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * from 0 to 9
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int num;
+
+	num = 0;
+	while (num < 10)
+		printf(num);
+		num ++;
+	return (0);
+}
