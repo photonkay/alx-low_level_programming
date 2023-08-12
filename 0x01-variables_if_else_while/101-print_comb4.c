6f2891e77ca0d1103c1e6df9fb55dea1678788d0
Prince <princeasa2005@gmail.com>
2023-08-12 05:13:46 +0000

    Every single time...

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,14 +8,14 @@ int main(void)
 {
 	int i, j, k;
 
-	for (i = 48; i <= 58; i++)
+	for (i = 48; i <= 55; i++)
 	{
-		for (j = 49; j <= 58; j++)
+		for (j = 49; j <= 56; j++)
 		{
-			for (k = 50; k <= 58; k++)
+			for (k = 50; k <= 57; k++)
 			{
 					
-				if (k > j && j > i && k > i)
+				if (k > j && j > i)
 				{
 					putchar(i);
 					putchar(j);
