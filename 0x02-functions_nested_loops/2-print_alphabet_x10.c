b996ad3f59be0fb2830b847015d50bc47e63d036
Prince <princeasa2005@gmail.com>
2023-08-15 22:26:43 +0000

    1-times 10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,17 +6,19 @@
  *
  * Return: 0 on success
  */
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
 	char ch;
 	int i;
 
 	for (i = 0; i < 10; i++)
+	{
 		while (ch <= 'c')
 		{
 			_putchar(ch);
 			ch ++;
 		}
 		_putchar('\n');
+	}
 }
 ~
