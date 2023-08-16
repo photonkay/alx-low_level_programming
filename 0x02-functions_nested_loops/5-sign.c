e3e5f5a5c86aa5d93775fb873c1a44a5d4792162
Prince <princeasa2005@gmail.com>
2023-08-16 01:26:21 +0000

    Signed?!

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,12 +11,18 @@
 int print_sign(int n)
 {
 	if (n > 0)
+	{
 		return (1);
 		_putchar('+');
+	}
 	else if (n < 0)
+	{
 		return (-1);
 		_putchar('-');
+	}
 	else
+	{
 		return (0);
 		_putchar('0');
+	}
 }
