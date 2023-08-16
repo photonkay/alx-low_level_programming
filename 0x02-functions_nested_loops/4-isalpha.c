2da41f5f3f8d16c9499b9d424beda901c48aaff8
Prince <princeasa2005@gmail.com>
2023-08-16 00:46:19 +0000

    Are you an alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _isalpha(int c) checks if char is an alphabet
+ * Return: 1 on success
+ * on error, 0 is returned
+ */
+int _isalpha(int c)
+{
+	if (c > 64 && c <= 90 || c > 96 && c <= 122)
+		return (1);
+	else
+		return (0);
+}
