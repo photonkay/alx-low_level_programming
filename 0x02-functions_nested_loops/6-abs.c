b9737b5eaec55f2ef28c78ae9ceaa608e0ff903e
Prince <princeasa2005@gmail.com>
2023-08-16 02:13:20 +0000

    Positive1

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,10 +3,10 @@
 
 /**
  * _abs - computes the absolute of an integer
- * @i is the integer passed
+ * @i: is the integer passed
  * Return: Always 0 on success
  */
-int _abs(int i) /*@i is the integer passed*/
+int _abs(int i) /*@i: is the integer passed*/
 {
 	if (i < 0)
 	{
