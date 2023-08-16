b2080210d73409eb193b83c1305472d970b7aa0c
Prince <princeasa2005@gmail.com>
2023-08-16 00:28:21 +0000

    Are you lower- 0

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,12 +2,12 @@
 #include "main.h"
 
 /**
- * @c: is the character to be tested
+ *c: is the character to be tested
  *function _islower(int c) - checks if an alphabet is lower
  * Return: Always 1 if c is lowercase
  * otherwise, 0 is returned
  */
-int _islower(int c) /*@c is the charater to be tested*/
+int _islower(int c) /*Checks lowercase c is the character to be tested*/
 {
 	if (c > 96 && c < 122)
 		return (1);
