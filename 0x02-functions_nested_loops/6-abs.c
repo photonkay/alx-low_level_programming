e302ff2624fd64c4629b2638fdab47b25762d265
Prince <princeasa2005@gmail.com>
2023-08-16 01:57:07 +0000

    Positive0

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,12 +3,11 @@
 
 /**
  * _abs: computes the absolute of an integer
+ * @i is the integer passed
  * Return: Always 0 on success
  */
-int _abs(int)
+int _abs(int i)
 {
-	int i; /*@i is the integer passed*/
-
 	if (i < 0)
 	{
 		i = -1 * i;
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,5 +2,5 @@
 #define main_h
 
 int _putchar(char c); 
-int _abs(int);
+int _abs(int i);
 #endif
