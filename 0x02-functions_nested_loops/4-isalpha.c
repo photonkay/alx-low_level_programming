3dadd370a37a9ebc1133d957f7ccdfb304ee55b7
Prince <princeasa2005@gmail.com>
2023-08-16 01:05:26 +0000

    Are you an alpha-33

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,7 +5,8 @@
  * Return: 1 on success
  * on error, 0 is returned
  */
-int _isalpha(int c) /*@c: number to pass to _isalpha*/
+
+int _isalpha(int c) /*@c: character to pass to _isalpha*/
 {
 	if (c > 64 && c <= 90)
 		return (1);
