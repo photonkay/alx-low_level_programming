ef4965a125e070d82eb04ef377726663fa94736f
Prince <princeasa2005@gmail.com>
2023-08-17 20:34:12 +0000

    Dashes

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_line - prints _ n times
+ * @n: number of times _ is printed
+ * Return: void
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
