95ee17594cdd913d87cfdcf78838e7f5d5705b03
Prince <princeasa2005@gmail.com>
2023-08-17 18:25:04 +0000

    Upper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,12 +2,11 @@
 #include "main.h"
 
 /**
- * -isupper checks for uppercase
+ * -isupper - checks for uppercase
  *  @c: is the letter passed into function
  *  Return: Always 1 if c is uppercase
  *  0 is returned otherwise
  */
-
 int _isupper(int c)
 {
 	if (c > 64 && c < 91)
