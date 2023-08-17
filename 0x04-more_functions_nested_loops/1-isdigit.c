fcee2953b8faaf049acd3a1be871a2f6e8e36b3c
Prince <princeasa2005@gmail.com>
2023-08-17 18:59:15 +0000

    Actually nested:

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isdigit - checks for a digit (0 through 9)
+ * @c: is the chracter passed
+ * Return: 1 if digit
+ * Return: 0 otherwise
+ */
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
