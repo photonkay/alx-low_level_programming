7bd9e0f1fabe01959c1570426b5eb44f1790cdf7
Prince <princeasa2005@gmail.com>
2023-08-17 19:24:35 +0000

    Numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_numbers - prints out numbers from 0 to 9
+ * no return
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(i);
+	}
+}
