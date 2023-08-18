dc45123cefc1640bfb3c8c2cab828a4dbb8b1e50
Prince <princeasa2005@gmail.com>
2023-08-18 00:41:49 +0000

    Upper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,19 +1,16 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * -isupper - checks for uppercase
- *  @c: is the letter passed into function
- *  Return: Always 1 if c is uppercase
+ * _isupper - uppercase letters
+ * @c: character to check
+ *
+ * Return: 0 or 1
  */
 int _isupper(int c)
+
 {
 	if (c >= 'A' && c <= 'Z')
-	{
 		return (1);
-	}
 	else
-	{
 		return (0);
-	}
 }
--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "main.h"
+#include <unistd.h>
 /**
  * main - prints the largest prime factor of 612852475143
  * 
