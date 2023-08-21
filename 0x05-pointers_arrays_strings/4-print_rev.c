faf0f969d4572517f63acc9bf99740a9c6029cc6
Prince <princeasa2005@gmail.com>
2023-08-21 18:10:33 +0000

    reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*print_rev - prints string in reverse
+*@str: string argument passed
+*Return: void
+*/
+void print_rev(char *str)
+{
+	int i, length = 0;
+
+	while (str[length] != '\0')
+		length++;
+	for (i = length - 1; i >= 0; i--)
+		putchar(str[i]);
+	putchar('\n');
+}
