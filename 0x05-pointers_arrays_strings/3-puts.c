533b2ae71685fe511b2be7eb8cfa1d7f0a7f8b44
Prince <princeasa2005@gmail.com>
2023-08-21 17:49:35 +0000

    Output to console

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+*_puts - prints out string
+*@str: string argument passed
+*Return: void
+*/
+void _puts(char *str)
+{
+	int i, length = 0;
+
+	while (str[length] != '\0')
+		length++;
+	for (i = 0; i <= length; i++)
+		putchar(str[i]);
+}
