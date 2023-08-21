6c9400d031b4d46f630639fb98c23066d2a3ba3b
Prince <princeasa2005@gmail.com>
2023-08-21 19:14:14 +0000

    Even positions

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+*puts2 - prints chars in even positions
+*@str: string argument passed
+*Return: void
+*/
+void puts2(char *str)
+{
+    int i, length = 0;
+
+	while (str[length] != '\0')
+		length++;
+
+	for (i = 0; i < length; i++)
+	{
+		if (i % 2 == 0)
+			putchar(str[i]);
+	}
+	putchar('\n');
+}
