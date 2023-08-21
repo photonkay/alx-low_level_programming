8d016b5d8b42ef2e384b19c457bb6b44c8f4ed37
Prince <princeasa2005@gmail.com>
2023-08-21 16:32:54 +0000

    First commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - sets integers passed to 98
+ * @*n: pointer pointing to address of integer value
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+Pointers store the memory address of variables
+Learning how to use pointers, arrays and array of characters (strings) 
