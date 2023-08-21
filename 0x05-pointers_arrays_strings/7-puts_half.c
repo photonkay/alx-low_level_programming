6c258d88fec0b585f126306a938f0f651372e8b0
Prince <princeasa2005@gmail.com>
2023-08-21 19:30:59 +0000

    Half of the whole

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,19 +7,19 @@
 */
 void puts_half(char *str)
 {
-    int i, length = 0;
+	int i, length = 0;
 
 	while (str[length] != '\0')
 		length++;
 
 	if (length % 2 == 0)
 	{
-		for (i = length/2; i < length; i++)
+		for (i = length / 2; i < length; i++)
 			_putchar(str[i]);
 	}
 	else
 	{
-		for (i = (length + 1)/2; i < length; i++)
+		for (i = (length + 1) / 2; i < length; i++)
 			_putchar(str[i]);
 	}
 	_putchar('\n');
