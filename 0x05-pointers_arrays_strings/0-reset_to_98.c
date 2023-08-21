1c52de8a66427a499693d4938928ab83aae2c51d
Prince <princeasa2005@gmail.com>
2023-08-21 16:34:51 +0000

    First commit..

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 
 /**
  * reset_to_98 - sets integers passed to 98
- * @*n: pointer pointing to address of integer value
+ * @n: pointer pointing to address of integer value
  * Return: void
  */
 void reset_to_98(int *n)
