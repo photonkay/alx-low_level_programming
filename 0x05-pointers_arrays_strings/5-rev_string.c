a0c26fec9bbb83e70775cc80eac0bc9b7bedf7d2
Prince <princeasa2005@gmail.com>
2023-08-22 14:43:11 +0000

    let's pull the reverse card

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,14 +7,13 @@
 */
 void rev_string(char *s)
 {
-	int i, start, end, length = 0;
+	int i, start, end, length;
 	char temp;
 
 	while (s[length] != '\0')
 		length++;
 
-	for (i = 0; i < (length - 1); i++)
-		end++;
+	end = length - 1;
 
 	while (start < end)
 	{
