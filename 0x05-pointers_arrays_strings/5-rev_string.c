91c34e889b2e32ea2e816199c2a99a9b5b3ae06e
Prince <princeasa2005@gmail.com>
2023-08-22 13:50:58 +0000

    Reverse string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,19 +7,22 @@
 */
 void rev_string(char *s)
 {
-	int start, length = 0;
+	int i, start, end, length = 0;
 	char temp;
 
 	while (s[length] != '\0')
 		length++;
 
-	while (start < (length - 1))
+	for (i = 0; i < (length - 1); i++)
+		end++;
+
+	while (start < end)
 	{
 		temp = s[start];
-		s[start] = s[(length - 1)];
-		s[(length - 1)] = temp;
+		s[start] = s[end];
+		s[end] = temp;
 
 		start++;
-		length--;
+		end--;
 	}
 }
