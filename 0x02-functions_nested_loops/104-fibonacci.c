41898fee25293a86828556b128be5958e61ed1c4
Prince <princeasa2005@gmail.com>
2023-08-23 01:29:39 +0000

    Always Fibonacci

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+*main - entry point
+*Description: program outputs Fibonacci numbers starting from 1 and 2
+*Return: always 0 on success
+*/
+int main(void)
+{
+        int i;
+        unsigned long int a = 1, b = 2, c = 0;
+
+        printf("1, 2, ");
+        for (i = 0; i < 96; i++)
+        {
+                c = a + b;
+                if (i < 95)
+                        printf("%lu, ", c);
+                else
+                        printf("%lu\n", c);
+                a = b;
+                b = c;
+        }
+        return (0);
+}
