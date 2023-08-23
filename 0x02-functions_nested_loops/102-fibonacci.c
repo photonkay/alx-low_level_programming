bc914ad04f560ec05395926831574246cb5a9bd6
Prince <princeasa2005@gmail.com>
2023-08-23 00:32:15 +0000

    Itz Fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+*main - entry point
+*Description: program outputs Fibonacci numbers starting from 1 and 2
+*Return: always 0 on success
+*/
+int main(void)
+{
+	int i, a = 1, b = 2;
+	long int c = 0;
+
+	printf("1, 2, ");
+	for (i = 0; i < 48; i++)
+	{
+		c = a + b;
+		if (i < 47)
+			printf("%llu, ", c);
+		else
+			printf("%llu", c);
+		a = b;
+		b = c;
+	}
+	return (0);
+}
