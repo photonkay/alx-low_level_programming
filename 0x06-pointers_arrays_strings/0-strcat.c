0463bcdab46f57c5765a4a1dd33ef1263d76f061
Prince <princeasa2005@gmail.com>
2023-08-23 18:33:45 +0000

    Concatenate

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+**_strcat - concatenates two strings
+*@dest: destination string
+*@src: string to be appended
+*Return: dest
+*/
+char *_strcat(char *dest, char *src)
+{
+	int i, len_1, len_2;
+
+	for (i = 0; dest[i] != '\0'; i++)
+		++len_1;
+	for (i = 0; src[i] != '\0'; i++)
+		++len_2;
+
+	for (i = 0; i < len_2; i++)
+		dest[len_1 + i] = src[i];
+
+	dest[len_1 + len_2] = '\0';
+	re`turn(dest);
+}
