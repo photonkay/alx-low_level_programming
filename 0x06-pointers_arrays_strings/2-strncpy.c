7f9997407cbe7ac6c452da1e13a5018272256957
Prince <princeasa2005@gmail.com>
2023-08-24 01:29:41 +0000

    Copy into another

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,8 +12,13 @@ char *_strncpy(char *dest, char *src, int n)
 	int i = 0;
 	char *s = dest;
 
-	while ((dest[i] = src[i]) != '\0')
-		++i;
+	while (n > 0 && *src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+		n--;
+	}
 
 	if (n == 0)
 	{
