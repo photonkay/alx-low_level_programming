5a322737705c6b5e14c3cca04799255d96f24355
Prince <princeasa2005@gmail.com>
2023-08-24 00:30:05 +0000

    Final push

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,8 +8,6 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	char *s = dest;
-
 	while (*dest != '\0')
 		dest++;
 
@@ -20,5 +18,5 @@ char *_strcat(char *dest, char *src)
 		src++;
 	}
 	*dest = '\0';
-	return (0);
+	return (dest);
 }
