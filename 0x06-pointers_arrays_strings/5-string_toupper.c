50585b1790469e2990e5481ef58a46ef9c1fbc46
Prince <princeasa2005@gmail.com>
2023-08-24 02:22:12 +0000

    Change to uppercase(LOOKUP)

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes lowercase to uppercase
+ * @s; string to be tested
+ * Return: char
+ */
+char *string_toupper(char *s)
+{
+	char *c = s;
+
+	while (*c != '\0')
+	{
+		if (*c >= 'a' && *c <= 'z')
+		{
+			*c = *c - ('a' - 'A');
+		}
+		*c++;
+	}
+	return (s);
+}
