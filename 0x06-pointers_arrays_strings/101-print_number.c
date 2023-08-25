efb01786a113b8b3e91073440b911597bb449656
Prince <princeasa2005@gmail.com>
2023-08-25 01:12:33 +0000

    Alx magic

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -4,11 +4,11 @@
   * print_number - Prints any integer with putchar
   * @n: Number to prints
   *
-  * Return: Nothing
+  * Return: void
   */
 void print_number(int n)
 {
-	int x;
+	int a;
 
 	if (n < 0)
 	{
