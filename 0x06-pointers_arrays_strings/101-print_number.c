1b41c5242fed27e421d81336c7e35e1cddc47e7e
Prince <princeasa2005@gmail.com>
2023-08-25 01:23:02 +0000

    Print numbers erh

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,18 +8,18 @@
   */
 void print_number(int n)
 {
-	int a;
+	int x;
 
 	if (n < 0)
 	{
 		_putchar('-');
 		n *= -1;
 	}
-
-	x = n;
+	else
+		x = n;
 
 	if (x / 10)
 		print_number(x / 10);
 
-	_putchar(x % 10 + '0');
+	_putchar((x % 10) + '0');
 }
