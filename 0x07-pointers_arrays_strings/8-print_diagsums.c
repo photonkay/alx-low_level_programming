a56b20ad50ea5b2d785149f24e3b2c34ad4cb128
Prince <princeasa2005@gmail.com>
2023-08-28 23:01:41 +0000

    Print diagonals

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -9,12 +9,13 @@
 */
 void print_diagsums(int *a, int size)
 {
-	int i, sum1, sum2 = 0;
+	int sum1 = 0;
+	int sum2 = 0;
 
-	for (i = 0; i < size; i++)
+	for (int i = 0; i < size; i++)
 	{
-		sum1 += *(a + i * size + i);
-		sum2 += *(a + i * size + (size - 1 - i));
+		sum1 += a[i * size + i];
+		sum2 += a[i * size + (size - 1 - i)];
 	}
 	printf("%d, %d\n", sum1, sum2);
 }
