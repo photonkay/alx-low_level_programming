0a933f6b0d1821bb61d2637b0c405a9ea6576cbd
Prince <princeasa2005@gmail.com>
2023-08-28 12:57:18 +0000

    First commit

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memset - fills memory with a constant byte
+ * @s: pointer to the string
+ * @b: constant byte
+ * @n: number of bytes 
+ */
+char *_memeset(char *s, char b, unsigned int n)
+{
+	char *p = *s;
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		*p = b;
+		*p++;
+	}
+
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,6 @@
+#ifndef main_h
+#define main_h
+
+char *memset(char *s, char b, unsigned int n);
+
+#endif
