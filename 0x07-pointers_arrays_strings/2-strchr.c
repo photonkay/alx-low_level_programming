7577f71aa8fa70df532f878418ab374dfdf666fc
Prince <princeasa2005@gmail.com>
2023-08-28 18:21:05 +0000

    Search and find

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,14 +9,15 @@
  */
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
+	unsigned int i;
+
+	for (i = 0; *(s + i) != '\0'; i++)
 	{
-		if (*s == c)
+		if (*(s + i) == c)
 		{
-			return (s);
+			return (s + i);
 		}
-		s++;
 	}
 
-	return (NULL);
+	return ('\0');
 }
