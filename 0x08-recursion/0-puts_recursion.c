7201a7a577088aead4203ad5ea2a48454a6c37ae
Prince <princeasa2005@gmail.com>
2023-08-30 19:27:32 +0000

    Very first commit

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - works as the puts function
+ * @s: string to print out
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	int i = 0;
+
+	if (s[i] != '\0')
+	{
+		_putchar(s[i]);
+		i ++;
+		s ++;
+		_puts_recursion(s);
+	}
+	else
+	{
+		_putchar('\0');
+		_putchar('\n');
+	}
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+So then lets go on. RECURSION!
