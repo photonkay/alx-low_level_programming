01cb0b7214c8f0696771a8f1fccc57999e3f89fc
Prince <princeasa2005@gmail.com>
2023-09-01 17:40:40 +0000

    Who am i

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,1 +1,13 @@
 #include <stdio.h>
+
+/**
+ * main - entry point
+ * Description - print name of program
+ * Return: always 0 success
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
