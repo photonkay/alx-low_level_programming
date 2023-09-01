b1bb803a0f92047e30cb935123dc8aa547c5f69e
Prince <princeasa2005@gmail.com>
2023-09-01 18:06:28 +0000

    Argument counting

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description - print number of arguments
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: always 0 success
+ */ 
+int main(int argc, char *argv[])
+{
+	if (*argv[0])
+		printf("%d\n", argc - 1);
+
+	return (0);
+}
