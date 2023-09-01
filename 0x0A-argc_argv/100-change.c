cffdbd1ea7e33979a605c53be9371143217c57af
Prince <princeasa2005@gmail.com>
2023-09-01 21:26:04 +0000

    How can i make change

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * Description - make change
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: always 0 success
+ */
+int main(int argc, char *argv[])
+{
+	int cents, counter;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	
+	cents = atoi(argv[1]);
+	if (cents < 0)
+	{
+		printf("0\n");
+	}
+
+	while (cents > 0)
+	{
+		if (cents >= 25)
+			cents -= 25;
+		else if (cents >= 10)
+			cents -= 10;
+		else if (cents >= 5)
+			cents -= 5;
+		else if (cents >= 2)
+			cents -= 2;
+		else if (cents >=1)
+			cents -= 1;
+		counter++;
+	}
+	printf("%d\n", counter);
+	return (0);
+}
