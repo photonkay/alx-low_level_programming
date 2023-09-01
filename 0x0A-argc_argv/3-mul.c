4e0636b465a10b5b96e582e4f3b84a39dd12c2a7
Prince <princeasa2005@gmail.com>
2023-09-01 18:38:40 +0000

    Multiply from command line

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ * Description - print number of arguments
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: always 0 success
+ */
+int main(int argc, char *argv[])
+{
+	int a, b, result;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	result = a * b;
+	
+	printf("%d\n", result);
+
+	return (0);
+}
