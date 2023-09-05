2df929d28bbdc23b988afa7ec56c3459dc65176b
Prince <princeasa2005@gmail.com>
2023-09-05 07:04:30 +0000

    Make a duplicate

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -13,11 +13,11 @@ char *_strdup(char *str)
 
 	dup = malloc(strlen(str) + 1);
 
-	if (dup == NULL)
+	if (str == NULL)
 	{
 		return (NULL);
 	}
-	if (str == NULL)
+	if (dup == NULL)
 	{
 		return (NULL);
 	}
