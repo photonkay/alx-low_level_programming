5b1806588a5befdf0587fcb0b023a391e7051ac9
Prince <princeasa2005@gmail.com>
2023-09-07 13:18:31 +0000

    Attenate

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,9 +14,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *dest;
 
 	if (s1 == NULL)
-		s1 == "";
+		s1 = "";
 	if (s2 == NULL)
-		s2 == "";
+		s2 = "";
 
 	len1 = strlen(s1);
 	len2 = strlen(s2);
