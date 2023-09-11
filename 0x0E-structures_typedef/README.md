b1a93e83e08dd0398b187fbaa0016630cb695df1
Prince <princeasa2005@gmail.com>
2023-09-11 22:38:54 +0000

    Dog particulars

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+Using struct and typedef
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,13 @@
+#ifndef DETAILS_H
+#define DETAILS_H
+
+/**
+ * dog - particulars of dog
+ */
+struct dog {
+	char* name;
+	float age;
+	char* owner;
+}
+
+#endif /* DETAILS_H */
