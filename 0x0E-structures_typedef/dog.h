d90de622d27918da216bb075f04c6f4ded59863b
Prince <princeasa2005@gmail.com>
2023-09-11 23:46:53 +0000

    Changes

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef _DOG_H
+#define _DOG_H
 
 /**
  * struct dog - particulars of dog
@@ -19,4 +19,4 @@ struct dog
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
-#endif /* DOG_H */
+#endif /* _DOG_H */
