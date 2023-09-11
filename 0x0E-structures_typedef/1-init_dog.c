d678a3559cee4e4ae3c1a22cae8625de4bc13bb3
Prince <princeasa2005@gmail.com>
2023-09-11 23:22:34 +0000

    Init

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,13 +1,12 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "dog.h"
 
 /**
- * init_dog - initialise dog params
- * @d: struct dog
- * @name: dog name
- * @age: dog age
- * @owner: dog owner
+ * init_dog - initialise dog params.
+ * @d: struct dog.
+ * @name: dog name.
+ * @age: dog age.
+ * @owner: dog owner.
+ *
  * Return: void
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
