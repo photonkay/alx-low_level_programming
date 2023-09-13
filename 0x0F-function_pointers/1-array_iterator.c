74da364c9e3c4df46d18cecd0ac89894a39b6db8
Prince <princeasa2005@gmail.com>
2023-09-13 13:58:42 +0000

    Iterate over some function

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+#include <stddef.h>
+
+/**
+ * array_iterator - iterates over a specific function
+ * @size: size of arry
+ * @action: action to perform during iteration
+ * Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (array && action)
+	{
+		for (i = 0; i < size; i++)
+			action(array[i]);
+	}
+}
