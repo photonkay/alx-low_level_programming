fd21c6796f2a6db1feecde3743de17fd438bfcb8
Prince <princeasa2005@gmail.com>
2023-09-25 23:17:11 +0000

    add node at beginning

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - adds a new node
+ * @head: pointer to header pointer
+ * @n: integer data
+ * Return: address of new node
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new = malloc(sizeof(struct listint_s));
+
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->next = *head; 
+	*head = new:
+
+	return (*head);
+}
