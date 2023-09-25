cab9a6a8eda5f7a3c1d4fc83302f50f8e382b966
Prince <princeasa2005@gmail.com>
2023-09-25 20:22:51 +0000

    Create 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,7 +12,7 @@ size_t print_list(const list_t *h)
 	while (h != NULL)
 	{
 		if (h->str == NULL)
-			printf("[0] (nil)");
+			printf("[0] (nil)\n");
 		printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 		count++;
