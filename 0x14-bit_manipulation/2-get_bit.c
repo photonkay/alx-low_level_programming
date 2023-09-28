d2646afb12cd66156dd945a2e8c3cf333bf1c2f9
Prince <princeasa2005@gmail.com>
2023-09-28 20:14:37 +0000

    Create 2-get_bit.c

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/** 
+ * get_bit - gets bit at specified index
+ * @n: the unsigned integer
+ * @index: specified index
+ * Return: bit
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int mask;
+	int bit;
+
+	if (index >= sizeof(n) * 8)
+		return (-1);
+
+	mask = 1UL << index;
+	bit = (n & mask) ? 1: 0;
+
+	return (bit);
+}
