63c0e99f9903b0ba41bff43b1ebe3296deb9cded
Prince <princeasa2005@gmail.com>
2023-09-28 18:58:21 +0000

    First commit, binary conversion

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts binary to unsigned int
+ * @b: character array of 0s and 1s
+ *Return: unsigned int
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int x = 0;
+
+	if (b == NULL)
+		return (0);
+
+	while(*b != '\0')
+	{
+		if (*b == '0' || *b == '1')
+		{
+			x = (x << 1) + (*b - '0');
+			b++;
+		}
+		else
+			return (0);
+	}
+	return (x);
+}
