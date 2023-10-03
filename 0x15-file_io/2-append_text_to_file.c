363d0b8a651612d0df971ea72d86121424f4f0aa
Prince <princeasa2005@gmail.com>
2023-10-03 02:33:31 +0000

    append to file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -18,7 +18,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	fd = open(filename, O_WRONLY | O_APPEND);
 
 	if (text_content == NULL)
-		return ;
+		;
 	else
 	{
 		characters = strlen(text_content);
