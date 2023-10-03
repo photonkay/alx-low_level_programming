e37fbdfa76390d93ae590341fd14691e387d3058
Prince <princeasa2005@gmail.com>
2023-10-03 02:18:07 +0000

    Create some file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file and writes into it
+ * @filename: name of file to create
+ * @text-content: content written into file
+ * Return: 1 on success
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int char_count = 0;
+	int fwrite;
+	
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+	if (text_content == NULL)
+		text_content = "";
+
+	char_count = strlen(text_content);
+	fwrite = write(fd, text_content, char_count);
+
+	if (fwrite == -1)
+		return (-1);
+
+	close(fd);
+	return (1);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,10 +6,11 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <string.h>
 
 /* function prototypes */
 ssize_t read_textfile(const char *filename, size_t letters);
-
+int create_file(const char *filename, char *textcontent);
 
 
 
